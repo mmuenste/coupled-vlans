982898a9da848f586ce822cf08421520c47f5e3c
Matze_M <m.muenstermann1@gmx.de>
2021-06-29 15:11:14 +0200

    Erster Commit: Das Tool und zwei Log-Files

---
+++ coupled_vlans.py
@@ -0,0 +1,52 @@
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('log_file', help='Specify the log file')
+
+args = parser.parse_args()
+
+root_ids = {}
+
+# Inhalt des Log-Files in Dictionary einlesen
+with open(args.log_file) as log:
+    for line in log:
+        line = line.strip().split()
+        if len(line) >= 3 and re.match('^[0-9a-f]{4,4}\.' +
+                                       '[0-9a-f]{4,4}\.[0-9a-f]{4,4}$',
+                                       line[2]):
+            root_ids[line[0]] = {'priority': line[1],
+                                 'mac': line[2],
+                                 'root_cost': line[3],
+                                 'hello_time': line[4],
+                                 'max_age': line[5],
+                                 'fwd_dly': line[6]
+                                 }
+            if len(line) == 8:
+                root_ids[line[0]]['root_port'] = line[7]
+            else:
+                root_ids[line[0]]['root_port'] = None
+
+# mehrfach auftauchende Root IDs ermittlen:
+# Liste mit Root IDs erzeugen
+root_id_list = [i['priority'] + ' ' + i['mac'] for i in root_ids.values()]
+
+# Mehrfach vorkommende Root IDs filtern
+multiple_root_ids = []
+for i in set(root_id_list):
+    if root_id_list.count(i) >= 2:
+        multiple_root_ids.append(i)
+
+# Ausgabe der mehrfach vorkommenden Root Ids
+if multiple_root_ids:
+    for i in multiple_root_ids:
+        prio_mac = i.split()
+        output = f'Root Bridge {prio_mac[0]} {prio_mac[1]} in VLANs: '
+        for vlan, data in root_ids.items():
+            if data['priority'] == prio_mac[0] and data['mac'] == prio_mac[1]:
+                output += f'{vlan} '
+        print(output)
+
+else:
+    print('No VLANs with the same ROOT ID found.')
